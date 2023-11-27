e8739193ec8c533ccfda7854d81739732ad5bf36
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-27 10:47:32 -0500

    Console.writeline  added

--- Program.cs
+++ Program.cs
@@ -3,7 +3,14 @@
 
 int age = 23;
 // age = "fifty"; // can't do this
+
 string name = "Benson";
 
-Console.WriteLine($"Your name is {name}, and you are {age} years old " );
+// Console.WriteLine($"Your name is {name}, and you are {age} years old " );
 
+
+BankAccount newAccount = new BankAccount();
+
+newAccount.Balance = 100.99;
+
+Console.WriteLine($"Your balance is {newAccount.Balance}");
--- obj/Debug/net8.0/dotnet-bank.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-bank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-bank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04413ecb482e1b48a2fca09088c863fe605c8e66")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-bank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-bank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
