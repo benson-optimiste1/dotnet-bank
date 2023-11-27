04413ecb482e1b48a2fca09088c863fe605c8e66
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-27 10:41:17 -0500

    BankAccount added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int age = 23;
+// age = "fifty"; // can't do this
+string name = "Benson";
+
+Console.WriteLine($"Your name is {name}, and you are {age} years old " );
+
